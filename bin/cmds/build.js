90f77e012fbe3393f61ccf7460dca40c1090ae95
Jonathan Dinu <inquiries@jonathan.industries>
2020-04-12 22:03:22 -0400

    build command to compile a project to stand alone html

---
+++ bin/cmds/build.js
@@ -0,0 +1,29 @@
+const { build } = require('../../src/handlers/build');
+
+exports.command = 'build';
+exports.desc =
+  'Compile markdown, MDX, or JSX/React files into a self contained bundle.';
+exports.builder = {
+  entry: {
+    alias: 'i',
+    default: './src/index.js',
+    describe: 'Input entry point.',
+    type: 'string',
+  },
+  output: {
+    alias: 'o',
+    default: './build',
+    describe: 'Output directory.',
+    type: 'string',
+  },
+  verbose: {
+    alias: 'v',
+    default: false,
+    describe: 'Display internal Webpack build logs.',
+    type: 'boolean',
+  },
+};
+
+exports.handler = async function (argv) {
+  build(argv.entry, argv.output, { verbose: argv.verbose });
+};
